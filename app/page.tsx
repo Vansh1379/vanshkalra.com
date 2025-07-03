5e6d92d9c65da2135405d5506fa647606550ce20
Vansh1379 <vanshkalra1379@gmail.com>
2025-07-04 00:45:03 +0530

    Add Navbar component and integrate it into the home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Navbar from "../components/navbar";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-zinc-900 min-h-screen antialiased">
+      <div className="">
+        <Navbar />
+      </div>
+    </div>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,20 @@
 const navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row items-center justify-between sm:justify-between py-8 px-8 max-w-5xl mx-auto relative z-[100]">
+      <div className="hidden lg:flex w-full justify-between">
+        <div className="">
+          <div className="">
+            <div>Photo</div>
+            <a href="">About</a>
+            <a href="">projects</a>
+            <a href="">Skills</a>
+            <a href="">Blogs</a>
+          </div>
+          <a className="">Download CV</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default navbar;
